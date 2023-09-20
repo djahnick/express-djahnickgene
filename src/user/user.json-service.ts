eb96a573a73f81d037153de42af4a31aebcddd9a
djahnickefrei <djagene@gmail.com>
2023-09-20 14:56:32 +0200

    express-1

--- src/user/user.json-service.ts
+++ src/user/user.json-service.ts
@@ -4,10 +4,27 @@ import * as fs from 'fs';
 
 export class UserJSONService implements UserService {
     private users: User[] = [];
+    private filePath: string = 'src/user/user.json'; // Assurez-vous que le chemin du fichier est correct
 
     constructor() {
-        const rawData = fs.readFileSync('src/user/user.json', 'utf-8');
-        this.users = JSON.parse(rawData);
+        this.loadUsersFromFile();
+    }
+
+    private loadUsersFromFile(): void {
+        try {
+            const rawData = fs.readFileSync(this.filePath, 'utf-8');
+            this.users = JSON.parse(rawData);
+        } catch (error) {
+            console.error("Une erreur s'est produite lors du chargement des utilisateurs depuis le fichier JSON :", error);
+        }
+    }
+
+    private saveUsersToFile(): void {
+        try {
+            fs.writeFileSync(this.filePath, JSON.stringify(this.users, null, 2), 'utf-8');
+        } catch (error) {
+            console.error("Une erreur s'est produite lors de l'écriture dans le fichier JSON :", error);
+        }
     }
 
     add(username: string): User {
@@ -15,7 +32,9 @@ export class UserJSONService implements UserService {
             id: this.users.length + 1,
             username,
         };
+
         this.users.push(newUser);
+        this.saveUsersToFile(); // Enregistrez les utilisateurs dans le fichier JSON
         return newUser;
     }
 
